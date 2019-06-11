de016e9929dc9486101adbce8f976a31204bd7d6
Gabriel Perez <gapc87@gmail.com>
2019-06-11 13:12:53 +0200

    role migration props added

--- database/migrations/2019_06_11_110056_create_roles_table.php
+++ database/migrations/2019_06_11_110056_create_roles_table.php
@@ -15,6 +15,9 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
