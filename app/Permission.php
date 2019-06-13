46ba4b71f3349b4b94d7c4661dc94bab79c2a3f2
Gabriel Perez <gapc87@gmail.com>
2019-06-13 20:16:39 +0200

    refactor models

--- app/Permission.php
+++ app/Permission.php
@@ -17,6 +17,6 @@ class Permission extends Model
 
     public function users()
     {
-        return $this->belongsToMany('App\User');
+        return $this->belongsToMany('App\User')->withTimestamps();
     }
 }
