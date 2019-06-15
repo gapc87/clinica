aa7aef8ce5a6cc7783b566b10af8ef3ba2dc1590
Gabriel Perez <gapc87@gmail.com>
2019-06-15 20:08:30 +0200

    Permission Policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Role' => 'App\Policies\RolePolicy',
+        'App\Permission' => 'App\Policies\PermissionPolicy',
     ];
 
     /**
